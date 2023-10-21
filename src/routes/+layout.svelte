12f007844b70ad7e9ed768bd8f47cfe24fd39367
Marci <mihmar17@gmail.com>
2023-10-21 14:57:10 +0200

    Add SEO

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+    import "../app.css";
+    import Modal from "$lib/components/Modals/Modal.svelte";
+</script>
+
+<svelte:head>
+    <meta charset="UTF-8" />
+    <title>Boilerplate | localhost.com</title>
+    <meta
+        name="keywords"
+        content="web3, web3 service, service, algorand service, algorand webservice, algo, algorand, algorand app"
+    />
+    <meta name="description" content="Service about..." />
+</svelte:head>
+<slot />
+<Modal />
