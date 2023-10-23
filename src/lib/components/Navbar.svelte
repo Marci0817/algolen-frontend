03a25497ff6f72a50f254bee39dfdafb859c67a2
Marci <mihmar17@gmail.com>
2023-10-23 17:01:03 +0200

    Create navbar, add new routing

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+    import ConnectButton from "$lib/components/ConnectButton.svelte";
+    import { getAllContexts } from "svelte";
+    import { fade } from "svelte/transition";
+
+    let isShowSubMenu: boolean = false;
+</script>
+
+<div class="flex justify-between items-center mx-20 mt-8 font-quicksand">
+    <div class="">
+        <h1 class="font-bold text-3xl">Fractic.</h1>
+        <p class="text-md text-gray-700">Best service</p>
+    </div>
+    <div>
+        <ul class="flex flex-row font-semibold">
+            <!-- svelte-ignore a11y-mouse-events-have-key-events -->
+            <!-- svelte-ignore a11y-no-static-element-interactions -->
+            <div
+                class="mx-4 flex-flex-col"
+                on:mouseover={() => {
+                    isShowSubMenu = true;
+                }}
+                on:mouseleave={() => {
+                    isShowSubMenu = false;
+                }}
+            >
+                <li class=" cursor-pointer">Fractionalize</li>
+                {#if isShowSubMenu}
+                    <div
+                        in:fade={{ duration: 100 }}
+                        class="absolute bg-gray-100 rounded-md p-2 px-4 drop-shadow-md font-medium"
+                    >
+                        <ul>
+                            <li>asd</li>
+                            <li>asd</li>
+                            <li>asd</li>
+                            <li>asd</li>
+                        </ul>
+                    </div>
+                {/if}
+            </div>
+            <li class="mx-4 cursor-pointer">Community</li>
+            <li class="mx-4 cursor-pointer">Support</li>
+            <li class="mx-4 cursor-pointer">Credits</li>
+        </ul>
+    </div>
+    <div>
+        <ConnectButton />
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
-    import ConnectButton from "$lib/components/ConnectButton.svelte";
+    import { goto } from "$app/navigation";
 </script>
 
-<p class="text-red-500 text-xl font-quicksand">This is the landing page!!!</p>
-<ConnectButton />
+<div class="flex flex-col justify-center items-center mt-60 font-quicksand">
+    <p class="text-red-500 text-xl">
+        Get started by clicking on the Launch button
+    </p>
+    <button
+        on:click={() => goto("/app")}
+        class="mt-4 py-2 px-5 rounded-lg bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 drop-shadow-lg text-white font-semibold"
+        >Launch app</button
+    >
+</div>
