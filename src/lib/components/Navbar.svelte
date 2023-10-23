75de8c1024c6e06c320e7abb86919031d800e68e
Marci <mihmar17@gmail.com>
2023-10-23 19:47:34 +0200

    Fetch assets

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,9 +1,49 @@
 <script lang="ts">
     import ConnectButton from "$lib/components/ConnectButton.svelte";
-    import { getAllContexts } from "svelte";
+    import { onMount } from "svelte";
     import { fade } from "svelte/transition";
+    import * as algosdk from "algosdk";
+    import { walletAddress } from "$lib/stores/walletStore";
+    import { env } from "$env/dynamic/public";
+
+    //types
+    import type LookupAssetByID from "algosdk/dist/types/client/v2/indexer/lookupAssetByID";
+    import type LookupAccountAssets from "algosdk/dist/types/client/v2/indexer/lookupAccountAssets";
 
     let isShowSubMenu: boolean = false;
+
+    const indexerClient = new algosdk.Indexer(
+        env.PUBLIC_ALGOSDK_TOKEN,
+        env.PUBLIC_ALGOSDK_SERVER,
+        env.PUBLIC_ALGOSDK_PORT
+    );
+
+    $: $walletAddress, fetchAssets();
+
+    let assetsName: string[] = [];
+
+    async function fetchAssets() {
+        if ($walletAddress) {
+            const HARD_CODED_ADDRESS =
+                "RSKWGMK7SMZG24WRUPNOXL6PHKOWW7TQNNHKXY72OXWYTQQORISCR2KHOA";
+            const accountAppLocalStates = await indexerClient
+                .lookupAccountAssets(HARD_CODED_ADDRESS) //TODO: Change this address to $walletAddress
+                .do();
+
+            accountAppLocalStates.assets.forEach(
+                async (item: LookupAccountAssets) => {
+                    const assetInfo: Record<string, any> = await indexerClient
+                        //@ts-ignore
+                        .lookupAssetByID(item["asset-id"]) // It should work with item.assetId but it cause error.
+                        .do();
+
+                    //if (assetInfo.asset.params.token === 1) { // TODO: Delete this comment
+                    assetsName = [...assetsName, assetInfo.asset.params.name];
+                    //}
+                }
+            );
+        }
+    }
 </script>
 
 <div class="flex justify-between items-center mx-20 mt-8 font-quicksand">
@@ -28,13 +68,12 @@
                 {#if isShowSubMenu}
                     <div
                         in:fade={{ duration: 100 }}
-                        class="absolute bg-gray-100 rounded-md p-2 px-4 drop-shadow-md font-medium"
+                        class="absolute bg-gray-100 rounded-md p-2 px-3 drop-shadow-md font-medium"
                     >
                         <ul>
-                            <li>asd</li>
-                            <li>asd</li>
-                            <li>asd</li>
-                            <li>asd</li>
+                            {#each assetsName as assetName}
+                                <li>{assetName}</li>
+                            {/each}
                         </ul>
                     </div>
                 {/if}
