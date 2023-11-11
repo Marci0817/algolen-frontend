d9669923c425bf073fda93ae49b69576c8a9495c
Marci <mihmar17@gmail.com>
2023-11-11 21:39:45 +0100

    Fix global error

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import commonjs from "@rollup/plugin-commonjs";
 
 export default defineConfig({
-    plugins: [sveltekit()],
+    plugins: [sveltekit(), commonjs()],
     optimizeDeps: {
         //For the wallets
         esbuildOptions: {
