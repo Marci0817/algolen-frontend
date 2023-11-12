1542ba3c9ce0552cdd1c93f2432638bd0fbb5a79
Sooki <0xSooki@gmail.com>
2023-11-12 09:51:35 +0100

    create card, button & search bar components

---
+++ src/lib/components/shared/Button.svelte
@@ -0,0 +1,13 @@
+<!-- NeonButton.svelte -->
+<script lang="ts">
+  export let onClick: () => void;
+  export let text: string;
+</script>
+
+<button
+  on:click={onClick}
+  class="bg-gray-900 text-white border-2 drop-shadow-neonPrim hover:drop-shadow-neon hover:border-sec border-prim font-primary rounded-2xl ml-2 py-2 px-3"
+  type="button"
+>
+  {text}
+</button>
