ee67d3282eda460e3694274ff3dc5c172af5ebbb
Marci <mihmar17@gmail.com>
2023-11-12 15:38:39 +0100

    Redesign navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,99 +1,93 @@
 <script lang="ts">
-  import ConnectButton from "$lib/components/ConnectButton.svelte";
-  import { onMount } from "svelte";
-  import { fade } from "svelte/transition";
-  import * as algosdk from "algosdk";
-  import { walletAddress } from "$lib/stores/walletStore";
-  import { env } from "$env/dynamic/public";
+    import ConnectButton from "$lib/components/ConnectButton.svelte";
+    import { fade } from "svelte/transition";
+    import NavbarItems from "$lib/components/NavbarItems.svelte";
 
-  //types
-  import type LookupAssetByID from "algosdk/dist/types/client/v2/indexer/lookupAssetByID";
-  import type LookupAccountAssets from "algosdk/dist/types/client/v2/indexer/lookupAccountAssets";
-
-  let isShowSubMenu: boolean = false;
-
-  const indexerClient = new algosdk.Indexer(
-    env.PUBLIC_ALGOSDK_TOKEN || "",
-    env.PUBLIC_ALGOSDK_SERVER,
-    env.PUBLIC_ALGOSDK_PORT
-  );
-
-  $: $walletAddress, fetchAssets();
-
-  let assetsName: string[] = [];
-
-  async function fetchAssets() {
-    if ($walletAddress) {
-      const HARD_CODED_ADDRESS =
-        "RSKWGMK7SMZG24WRUPNOXL6PHKOWW7TQNNHKXY72OXWYTQQORISCR2KHOA";
-      const accountAppLocalStates = await indexerClient
-        .lookupAccountAssets(HARD_CODED_ADDRESS) //TODO: Change this address to $walletAddress
-        .do();
-
-      accountAppLocalStates.assets.forEach(
-        async (item: LookupAccountAssets) => {
-          const assetInfo: Record<string, any> = await indexerClient
-            //@ts-ignore
-            .lookupAssetByID(item["asset-id"]) // It should work with item.assetId but it cause error.
-            .do();
-
-          //if (assetInfo.asset.params.total === 1 && assetInfo.asset.params.decimals === 0) { // TODO: Delete this comment
-          assetsName = [...assetsName, assetInfo.asset.params.name];
-          //}
-        }
-      );
-    }
-  }
+    let isSideBarOpen = false;
 </script>
 
 <div
-  class="flex justify-between items-center mx-20 sm:mt-8 mt-2 font-primary text-white"
+    class="flex justify-between items-center mx-16 md:mx-10 mt-10 font-primary text-white"
 >
-  <div class="">
-    <h1
-      class="font-extrabold text-4xl bg-clip-text bg-gradient-to-r text-transparent from-prim to-sec"
-    >
-      <p class="sm:hidden block">FRC</p>
-      <p class="hidden sm:block">Fractic</p>
-    </h1>
-  </div>
-  <div>
-    <ul class="flex flex-row font-medium">
-      <!-- svelte-ignore a11y-mouse-events-have-key-events -->
-      <!-- svelte-ignore a11y-no-static-element-interactions -->
-      <div
-        class="mx-4 flex-flex-col"
-        on:mouseover={() => {
-          isShowSubMenu = true;
-        }}
-        on:mouseleave={() => {
-          isShowSubMenu = false;
-        }}
-      >
-        <li class=" cursor-pointer">Fractionalize</li>
-
-        {#if isShowSubMenu}
-          <div
-            in:fade={{ duration: 100 }}
-            class="absolute bg-gray-100 rounded-md p-2 px-3 drop-shadow-md font-medium"
-          >
-            <ul>
-              {#if assetsName.length > 0}
-                {#each assetsName as assetName}
-                  <li>{assetName}</li>
-                {/each}
-              {:else}
-                <li class="text-black">You dont have assets</li>
-              {/if}
-            </ul>
-          </div>
-        {/if}
-      </div>
-      <li class="mx-4 cursor-pointer">Community</li>
-      <li class="mx-4 cursor-pointer">Support</li>
-    </ul>
-  </div>
-  <div>
-    <ConnectButton />
-  </div>
+    <div class="flex justify-center items-center">
+        <div class="mr-8">
+            <h1
+                class="font-extrabold text-4xl bg-clip-text bg-gradient-to-r text-transparent from-prim to-sec"
+            >
+                <p class="md:hidden block">Algolen</p>
+                <p class="hidden md:block">Algolen</p>
+            </h1>
+        </div>
+        <div class="hidden md:block">
+            <NavbarItems />
+        </div>
+        <div class="absolute right-12 md:hidden">
+            <button
+                on:click={() => {
+                    isSideBarOpen = !isSideBarOpen;
+                }}
+            >
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                    />
+                </svg></button
+            >
+        </div>
+    </div>
+    <div class="md:block hidden">
+        <ConnectButton />
+    </div>
 </div>
+
+<aside
+    class="absolute w-full h-full left-0 top-0 bg-gray-200 border-r-2 shadow-lg"
+    class:isSideBarOpen
+>
+    <nav class="p-12 text-xl">
+        <div class="absolute right-12 md:hidden">
+            <button
+                on:click={() => {
+                    isSideBarOpen = !isSideBarOpen;
+                }}
+            >
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M6 18L18 6M6 6l12 12"
+                    />
+                </svg>
+            </button>
+        </div>
+        <NavbarItems />
+        <ConnectButton />
+    </nav>
+</aside>
+
+<style>
+    aside {
+        left: -100%;
+        transition: left 0.3s ease-in-out;
+    }
+
+    .isSideBarOpen {
+        left: 0;
+    }
+</style>
