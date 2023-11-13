f6660f51a99e62c013075a8a590d16dfbb8eaf36
Marci <mihmar17@gmail.com>
2023-11-13 15:54:37 +0100

    Redesign landing page, add responsive design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,33 +4,32 @@
 </script>
 
 <div
-    class="flex flex-col justify-center items-center mt-32 font-primary mx-auto"
+    class="flex flex-col mt-32 font-primary mx-auto md:items-start items-center max-w-5xl"
 >
     <h1
-        class="font-extrabold text-6xl bg-clip-text bg-gradient-to-r text-transparent from-prim to-sec"
+        class="font-extrabold text-8xl bg-clip-text bg-gradient-to-r text-transparent from-prim to-sec"
     >
         <p class="">Algolen</p>
     </h1>
     <p
-        class="text-prim font-semibold text-justify mx-10 md:mx-0 md:w-1/2 text-xl mt-16"
+        class="text-prim font-medium text-xl mt-16 md:mx-0 mx-10 md:text-left text-center max-w-2xl"
     >
         Lorem Ipsum is simply dummy text of the printing and typesetting
         industry. Lorem Ipsum has been the industry's standard dummy text ever
         since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
+        scrambled it to make a type specimen book. It has survived not.
     </p>
-    <button
-        on:click={() => goto("/app")}
-        class="py-3 px-8 rounded-lg drop-shadow-neon text-white font-bold bg-sec mt-12"
-        >Launch app</button
-    >
-    <h1 class="font-bold text-white text-4xl mt-32 text-center">
+    <div class="flex items-center mt-12 gap-6">
+        <button
+            on:click={() => goto("/app")}
+            class="py-3 px-8 rounded-lg drop-shadow-neon text-white font-bold bg-sec"
+            >Launch app</button
+        >
+    </div>
+    <h1 class="font-bold text-white text-4xl mt-32 text-center md:mx-auto mx-0">
         How NFT renting works
     </h1>
+</div>
+<div class="mb-32">
     <FlowChart />
 </div>
