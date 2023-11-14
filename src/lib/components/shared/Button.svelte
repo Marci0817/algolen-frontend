b5817bd83f5503acc95e10eb384c976dcb290973
Marci0817 <mihmar17@gmail.com>
2023-11-14 10:03:12 +0100

    Update Button.svelte

--- src/lib/components/shared/Button.svelte
+++ src/lib/components/shared/Button.svelte
@@ -8,7 +8,7 @@
 <button
     on:click={onClick}
     {disabled}
-    class={`bg-gray-900 text-white border-2 drop-shadow-neonPrim hover:drop-shadow-neon hover:border-sec border-prim font-primary rounded-2xl ml-2 py-2 px-3 ${className}}`}
+    class={`bg-gray-900 text-white border-2 drop-shadow-neonPrim hover:drop-shadow-neon hover:border-sec border-prim font-primary rounded-lg ml-2 py-1 px-2 ${className}}`}
     type="button"
 >
     {text}
