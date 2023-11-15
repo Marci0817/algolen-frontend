7acf5c9c07b17cdeed8255f2e81a0376d44b6eed
Marci0817 <mihmar17@gmail.com>
2023-11-16 00:39:07 +0100

    Last design fix, add more context

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,20 +4,35 @@
 </script>
 
 <div
-    class="flex flex-col mt-32 font-primary mx-auto md:items-start items-center max-w-5xl"
+    class="flex flex-col mt-16 md:mt-32 font-primary mx-auto md:items-start items-center max-w-5xl"
 >
-    <h1
-        class="font-extrabold text-8xl bg-clip-text bg-gradient-to-r text-transparent from-prim to-sec"
-    >
-        <p class="">Algolen</p>
-    </h1>
-    <p
+        
+    <div>
+        <h1
+            class="font-extrabold text-8xl bg-clip-text bg-gradient-to-r text-transparent from-prim to-sec"
+        >
+            <p class="">Algolen</p>
+        </h1>
+        <p class="text-sec text-3xl mt-3 text-center md:text-left">NFT lending & renting</p>
+    </div>
+    <div
         class="text-prim font-medium text-xl mt-16 md:mx-0 mx-10 md:text-left text-center max-w-2xl"
     >
-    Imagine a world where you can lend out your unused NFTs to generate passive income and rent out NFTs for short term use. <br><br>
-    You just joined a new NFT based game on Algorand, you are enjoying the game but don't have much time to play. Don't worry because at Algolen you can rent out a high level NFT and enjoy the endgame at your leisure. <br><br>
-    No need for long term commitments, renting is a fraction of the cost of buying the NFT at full price.
-    </p>
+        <div class="">
+            <h2 class="font-semibold text-white uppercase mb-1">Passive income</h2>
+            <p>Imagine a world where you can lend out your unused NFTs to generate passive income and rent out NFTs for short term use.</p>
+        </div>
+        <div class="text-center md:text-right my-5">
+            <h2 class="font-semibold text-white uppercase">Gaming</h2>
+            <p class="md:marker:text-justify">You just joined a new NFT based game on Algorand, you are enjoying the game but don't have much time to play. Don't worry because at Algolen you can rent out a high level NFT and enjoy the endgame at your leisure.</p>
+        </div>
+        <div class="">
+            <h2 class="font-semibold text-white uppercase ">No commitments</h2>
+            <p>No need for long term commitments, renting is a fraction of the cost of buying the NFT at full price.</p>
+        </div>
+        
+        
+    </div>
     <div class="flex items-center mt-12 gap-6">
         <button
             on:click={() => goto('/rent')}
